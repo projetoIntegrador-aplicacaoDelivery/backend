98780832d487cd4467abd6928c3a3eb6d005489e
Daniel Lima <dsl.daniel.santos.lima@gmail.com>
2024-12-10 16:30:58 -0300

    Produto Controller

--- AplicacaoDelivery/AplicacaoDelivery/src/main/java/com/generation/AplicacaoDelivery/repository/ProdutoRepository.java
+++ AplicacaoDelivery/AplicacaoDelivery/src/main/java/com/generation/AplicacaoDelivery/repository/ProdutoRepository.java
@@ -1,12 +1,17 @@
 package com.generation.AplicacaoDelivery.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.AplicacaoDelivery.model.Produto;
 
 @Repository
 	public interface ProdutoRepository extends JpaRepository <Produto, Long>{
+
+	List<Produto> findAllByNomeProdutoContainingIgnoreCase(@Param("nomeProduto") String nomeProduto);
 
 
 }
