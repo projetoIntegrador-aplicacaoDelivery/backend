5eee59aee9aa1046b72346d5bcbf92064ec3af6c
sabrinasanmi <sanmisabrina@icloud.com>
2024-12-10 11:14:50 -0300

    Criado aplicação

---
+++ AplicacaoDelivery/AplicacaoDelivery/src/test/java/com/generation/AplicacaoDelivery/AplicacaoDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.AplicacaoDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
