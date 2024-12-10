49047159a3eed16d8f2874bf0fcf5fbd1b021443
adanisantana <mariano.dani@hotmail.com>
2024-12-10 11:50:43 -0300

    Classe categoria model

---
+++ AplicacaoDelivery/AplicacaoDelivery/src/main/java/com/generation/AplicacaoDelivery/model/Categoria.java
@@ -0,0 +1,49 @@
+package com.generation.AplicacaoDelivery.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	
+	private String tipo;
+	
+	private List<Produtos> produtos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public List<Produtos> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produtos> produtos) {
+		this.produtos = produtos;
+	}
+	
+	
+
+}
