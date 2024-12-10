b1b7475fb466d172d1000adfff0fe0fd5594cffd
rodrigo oliveira <pracimaestudos@gmail.com>
2024-12-10 14:49:39 -0300

    tarefa repository

---
+++ AplicacaoDelivery/AplicacaoDelivery/src/main/java/com/generation/AplicacaoDelivery/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.generation.AplicacaoDelivery.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.AplicacaoDelivery.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario , Long> {
+	
+	
+
+}
