9dcb553be64b2f08ae882f70a445807b9aa615ab
Daniel Lima <dsl.daniel.santos.lima@gmail.com>
2024-12-10 11:57:11 -0300

    Classe Produto

---
+++ AplicacaoDelivery/AplicacaoDelivery/src/main/java/com/generation/AplicacaoDelivery/model/Produto.java
@@ -0,0 +1,73 @@
+package com.generation.AplicacaoDelivery.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank (message = "O atributo 'Nome' não pode estar vazio.")
+	@Size (min = 3, max = 255, message = "O atributo 'nome' deve ter entre 3 e 255 caracteres.")
+	private String nomeProduto;
+	
+	@NotBlank (message = "O atributo 'Descrição' não pode estar vazio.")
+	@Size (min = 5, max = 255, message = "O atributo 'nome' deve ter entre 5 e 255 caracteres.")
+	private String descricao;
+	
+	@NotNull (message = "O atributo 'Preço' não pode estar vazio.")
+	private Float preco;
+	
+	private String foto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+		
+}
