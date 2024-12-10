354083bf4edb80a41e35846165d71e8bc1905650
adanisantana <mariano.dani@hotmail.com>
2024-12-10 14:21:34 -0300

    Categoria Repository

--- AplicacaoDelivery/AplicacaoDelivery/src/main/java/com/generation/AplicacaoDelivery/repository/CategoriaRepository.java
+++ AplicacaoDelivery/AplicacaoDelivery/src/main/java/com/generation/AplicacaoDelivery/repository/CategoriaRepository.java
@@ -1,8 +1,17 @@
 package com.generation.AplicacaoDelivery.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.generation.AplicacaoDelivery.model.Categoria;
+
 @Repository
-public interface CategoriaRepository {
-	
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	public List<Categoria> findAllByTipoContainingIgnoreCase(@Param("tipo")String tipo);
 
 
 }
+ 
